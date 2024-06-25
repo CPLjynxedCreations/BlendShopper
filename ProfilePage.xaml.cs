8de063405fb4cc298bdc9737b9110ed2e801c193
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-06-25 17:37:43 +1000

    Profile page made with store and profile button working

--- ProfilePage.xaml.cs
+++ ProfilePage.xaml.cs
@@ -20,21 +20,21 @@ namespace BlendShopper
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class ProfilePage : Page
     {
-        public MainPage()
+        public ProfilePage()
         {
             this.InitializeComponent();
-        }
-
-        private void btn_Profile_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(ProfilePage));
         }
 
         private void btn_Exit_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void btn_Shop_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
